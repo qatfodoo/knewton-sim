ec598acba3835bafda226327dad39f6598754035
Kuate Kevin Fodouop <kfodouop@holy2a18305.rc.fas.harvard.edu>
2014-10-09 11:18:54 -0400

    Pulling.

--- knewton_sim.py
+++ knewton_sim.py
@@ -6,4 +6,8 @@ top_users_quest = pickle.load( open( "top_users_quest.pkl", "rb" ) )
 
 top_users_perf = pickle.load( open( "top_users_perf.pkl", "rb" ) )
 
+print type(top_users)
+print type(top_users_quest)
+print type(top_users_perf)
+
 print top_users_quest[350]
