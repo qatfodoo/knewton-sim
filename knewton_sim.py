69f276a902111c4c7c4d50fd303842ae8febc50b
qatfodoo <kevin.kuate-fodouop@polytechnique.edu>
2014-10-09 10:23:44 -0400

    added slurm file.

--- knewton_sim.py
+++ knewton_sim.py
@@ -6,4 +6,4 @@ top_users_quest = pickle.load( open( "top_users_quest.pkl", "rb" ) )
 
 top_users_perf = pickle.load( open( "top_users_perf.pkl", "rb" ) )
 
-print "done loading"
+print top_users_quest[350]
