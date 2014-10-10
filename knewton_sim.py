a63697910709ea81475f7ba0a7633c20d3d57ce9
qatfodoo <kevin.kuate-fodouop@polytechnique.edu>
2014-10-10 18:12:54 -0400

    Pulling

--- knewton_sim.py
+++ knewton_sim.py
@@ -1,5 +1,6 @@
 import pickle
 import itertools
+import random
 from collections import defaultdict
 
 top_users = list(pickle.load( open( "top_users.pkl", "rb" ) ))
@@ -8,34 +9,34 @@ top_users_quest = pickle.load( open( "top_users_quest.pkl", "rb" ) )
 
 top_users_perf = pickle.load( open( "top_users_perf.pkl", "rb" ) )
 
-print type(top_users)
-print type(top_users_quest)
-print type(top_users_perf)
-
-print top_users_quest[350]
+M = 10000 # Number of simulations per top users
 
 avg_err = defaultdict(int) # Avg error of combinations involving question
 n_comb = defaultdict(int) # Number of comb having each quest (avoid computing combinatorics (needs unavailable packages))
 
-i = 0
-for u in top_users[0:1]:
-    i += 1
-    print i # Count of users in the loop
+ov_err = 0 # Overall avg error on all simulations
+ov_it = 0
+for u in top_users:
 
     quest = top_users_quest[u].keys()
-    top_users_questcomb = list(itertools.combinations(quest, 5))
-    user_perf = top_users_perf[u]
+    user_perf = top_users_perf[u][0]
 
-    print user_perf
-    
-    for c in top_users_questcomb:
-    	err = (user_perf - 1 / len(c) * float(sum(c))) ** 2 # quadratic error
+    for b in range(M):
+    	c = random.sample(quest, 5)
+    	err = (user_perf - 1 / float(len(c)) * float(sum([top_users_quest[u][q] for q in c]))) ** 2 # quadratic error
+        ov_err += err
+        ov_it += 1
+
     	for q in c:	
 	    	avg_err[q] += err
 	    	n_comb[q] += 1
 
-	for q in avg_err.keys():
-		avg_err[q] /= n_comb[q]
+
+
+for q in avg_err.keys():
+	avg_err[q] /= n_comb[q]
 
 print avg_err
-    
+
+print "Overall avg error"
+print ov_err / ov_it
