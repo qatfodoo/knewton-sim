a4af060bffb5c90027f126dc8e15f41bd2165338
qatfodoo <kevin.kuate-fodouop@polytechnique.edu>
2014-10-11 13:24:48 -0400

    Fixed some shit.

--- knewton_sim_select.py
+++ knewton_sim_select.py
@@ -6,8 +6,7 @@ import time
 import math
 
 if os.getenv('SLURM_JOB_ID') != "":
-    #task_id = int(os.getenv('SLURM_ARRAY_TASK_ID')) # Task id, to parallelize computation
-    task_id = 10
+    task_id = int(os.getenv('SLURM_ARRAY_TASK_ID')) # Task id, to parallelize computation
     print task_id
     start_time = time.time()
 
@@ -22,9 +21,6 @@ if os.getenv('SLURM_JOB_ID') != "":
     # Load questions ordered by scoring error in previous simulation
     sorted_quest = pickle.load(open("./out/sorted_quest.pkl", "rb")) # tuple
     max_ind = int(math.floor(len(sorted_quest) * (0.5 + float(task_id) / 2 / N_tasks))) # From 50% to all questions.
-    print max_ind
-    print 
-    print len(sorted_quest)
     final_bank = [t[0] for t in sorted_quest][0:max_ind]
 
     sum_err = defaultdict(int) # Avg error of combinations involving question
